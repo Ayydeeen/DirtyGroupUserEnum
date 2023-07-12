c15a2b60e1b3f0937ad5f2d123549f192880dfd4
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2023-07-12 11:14:57 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 Get-ADGroup -filter * | sort-object name|select-object name | export-csv C:\Temp\groups.csv
 
-gc C:\Temp\Groups.csv) | % {$_ -replace '"',""} | out-file C:\Temp\Groupsq.csv -Fo -En ascii
+gc C:\Temp\Groups.csv | % {$_ -replace '"',""} | out-file C:\Temp\Groupsq.csv -Fo -En ascii
 
 get-content C:\Temp\groupsq.csv | foreach {Add-Content -path C:\temp\test3.txt -Value $_ ; Get-AdGroupMember -identity $_ | Select Name >> C:\Temp\test3.txt }
